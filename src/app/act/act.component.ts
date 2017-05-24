a1b44634d1366c7c2ade7a181dd5951f3eb42663
Isto Barton <istobarton@gmail.com>
2017-05-23 22:17:20 -0500

    fix highlighting issue, add headshot

--- src/app/act/act.component.ts
+++ src/app/act/act.component.ts
@@ -27,10 +27,14 @@ import { ActivatedRoute } from '@angular/router';
       </div>
       <div class="links links-neon">
         <a href="mailto:istobarton@gmail.com">email</a>
-        <!--<a href="https://docs.google.com/document/d/16AUDylQAXch03o_ASd5hE6NTGCn19mIq2NtNA8E4O0k/edit?usp=sharing" target="_blank">resume</a>-->
-        <!--<a href="">images</a>-->
-        <!--<a href="">videos</a>-->
+        <a href="https://drive.google.com/file/d/0BzoHrXVpe37cbmdkNlZmSnJlVlk/view?usp=sharing" target="_blank">headshot</a>
+        <!--<a href="" target="_blank">resume</a>-->
       </div>
+      <!--<div class="links links-neon">-->
+        <!--<a href="" target="_blank">photo</a>-->
+        <!--<a href="" target="_blank">video</a>-->
+        <!--<a href="" target="_blank">audio</a>-->
+      <!--</div>-->
     </div>
   `
 })
